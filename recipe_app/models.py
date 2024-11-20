25d68688604e807ae0a3e0907157f7f99cc3543d
dragon <basselt@abv.bg>
2024-11-20 17:41:59 +0200

    third commit

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Chef(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    chef = models.ForeignKey(Chef, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    instructions = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    chef = models.ForeignKey(Chef, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
